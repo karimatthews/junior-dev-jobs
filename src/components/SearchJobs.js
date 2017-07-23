a2326761be7d7207b9ef821093c6cee93fbdec71
Kari Matthews <karimatthews3@gmail.com>
2017-07-23 21:41:14 +1000

    Fetch data from server

--- src/components/SearchJobs.js
+++ src/components/SearchJobs.js
@@ -1,14 +1,37 @@
 import { Grid, Jumbotron, Button } from 'react-bootstrap';
-import React from 'react'
+import React, {Component} from 'react'
 
-const SearchJobs = () => (
+class SearchJobs extends Component{
+
+  constructor() {
+    super()
+    this.state = {
+      jobs: []
+    }
+  }
+
+  componentWillMount() {
+    fetch('http://localhost:3000/jobs').then((response) => {
+      return response.json()
+    }).then((json) => {
+      this.setState({ jobs: json })
+    })
+  }
+
+  render() {
+  return (
   <div>
     <Jumbotron>
       <Grid>
         <h2>Search Jobs</h2>
+        {
+            this.state.jobs.map((job, i) => (
+              <h3 key={i}>{ job.title }</h3>
+            ))
+        }
       </Grid>
     </Jumbotron>
   </div>
-)
+)}}
 
 export default SearchJobs
