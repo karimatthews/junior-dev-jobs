5df06930a56acd22003577a554fdcdf189fece80
Kari Matthews <karimatthews3@gmail.com>
2017-07-24 16:13:21 +1000

    Add jobs to search jobs page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import {
   Link
 } from 'react-router-dom'
 
+import './App.css';
+import './SearchJobs.css';
+
 //import components
 import Topbar from './components/Topbar'
 
--- src/components/SearchJobs.js
+++ src/components/SearchJobs.js
@@ -24,13 +24,25 @@ class SearchJobs extends Component{
     <Jumbotron>
       <Grid>
         <h2>Search Jobs</h2>
-        {
-            this.state.jobs.map((job, i) => (
-              <h3 key={i}>{ job.title }</h3>
-            ))
-        }
+
       </Grid>
     </Jumbotron>
+
+
+    {/* Display Jobs */}
+    <div className = 'container container--vertical'>
+      {
+          this.state.jobs.map((job, i) => (
+            <div key={i} className = 'job'>
+              <h3>{ job.title }</h3>
+              <strong>{ job.company }</strong>
+              <p>{ job.suburb}</p>
+              <p>${ job.formatted_salary}</p>
+              <p>{ job.description}</p>
+            </div>
+          ))
+      }
+    </div>
   </div>
 )}}
 
