28509533a59d749891d5524ca6f3fa71f0743636
Kari Matthews <karimatthews3@gmail.com>
2017-07-28 22:00:04 +1000

    Filter by work type

--- src/components/SearchJobs.js
+++ src/components/SearchJobs.js
@@ -6,7 +6,8 @@ class SearchJobs extends Component{
   constructor() {
     super()
     this.state = {
-      jobs: []
+      jobs: [],
+      work_types: {internship: true, fulltime: true, parttime: true, casual: true, contract: true}
     }
   }
 
@@ -14,7 +15,7 @@ class SearchJobs extends Component{
     fetch('http://localhost:3000/jobs').then((response) => {
       return response.json()
     }).then((json) => {
-      this.setState({ jobs: json })
+      this.setState({ jobs: json.reverse() })
     })
   }
 
@@ -28,24 +29,79 @@ class SearchJobs extends Component{
       </Grid>
     </Jumbotron>
 
-
     {/* Display Jobs */}
-    <div className = 'container container--vertical'>
-      {
-          this.state.jobs.reverse().map((job) => (
-            <div key={job.id} className = 'job container container--vertical'>
-              <div className = 'toprow'>
-                <p className = 'title'>{ job.title }</p>
-                <p className = "topright"> { job.work_type}</p>
+
+    <div className = 'container'>
+      <div className = 'container container--vertical filter'>
+        <h3>Filter and refine</h3>
+        <p> Search </p>
+        <p> Work type: </p>
+
+        <div className="checkbox">
+          <label><input type="checkbox" checked={this.state.work_types.internship} onChange = {
+            (event) => {
+              this.setState({work_types: { ...this.state.work_types, internship: event.target.checked}})
+            }
+          }/> Internship</label>
+        </div>
+
+        <div className="checkbox">
+          <label><input type="checkbox" checked={this.state.work_types.fulltime} onChange = {
+            (event) => {
+              this.setState({work_types: { ...this.state.work_types, fulltime: event.target.checked}})
+            }
+          }/> Full Time</label>
+        </div>
+
+        <div className="checkbox">
+          <label><input type="checkbox" checked={this.state.work_types.parttime} onChange = {
+            (event) => {
+              this.setState({work_types: { ...this.state.work_types, parttime: event.target.checked}})
+            }
+          }/> Part Time</label>
+        </div>
+
+        <div className="checkbox">
+          <label><input type="checkbox" checked={this.state.work_types.contract} onChange = {
+            (event) => {
+              this.setState({work_types: { ...this.state.work_types, contract: event.target.checked}})
+            }
+          }/> Contract</label>
+        </div>
+
+        <div className="checkbox">
+          <label><input type="checkbox" checked={this.state.work_types.casual} onChange = {
+            (event) => {
+              this.setState({work_types: { ...this.state.work_types, casual: event.target.checked}})
+            }
+          }/> Casual</label>
+        </div>
+
+        <p> Salary range </p>
+        <p> Location </p>
+        <p>Suburb </p>
+
+      </div>
+
+      <div className = 'container container--vertical'>
+        {
+            this.state.jobs.filter((job) => {
+              return this.state.work_types[job.work_type.split(' ').join('')]
+            }).map((job) => (
+              <div key={job.id} className = 'job container container--vertical'>
+                <div className = 'toprow'>
+                  <p className = 'title'>{ job.title }</p>
+                  <p className = "topright"> { job.work_type}</p>
+                </div>
+                <p> <strong>{ job.company }</strong> - { job.suburb}</p>
+                {
+                  job.formatted_salary && <p>Salary: ${ job.formatted_salary}</p>
+                }
+                <p>{ job.description}</p>
               </div>
-              <p> <strong>{ job.company }</strong> - { job.suburb}</p>
-              {
-                job.formatted_salary && <p>Salary: ${ job.formatted_salary}</p>
-              }
-              <p>{ job.description}</p>
-            </div>
-          ))
-      }
+            ))
+        }
+      </div>
     </div>
   </div>
 )}}
