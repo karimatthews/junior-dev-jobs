d99d51d1fa34c2bdb223be3ad5b748470ab1874e
Kari Matthews <karimatthews3@gmail.com>
2017-08-01 22:19:32 +1000

    show short version of description by default

---
+++ src/components/JobItem.js
@@ -0,0 +1,53 @@
+import React, {Component}  from 'react'
+import TimeStamp from 'react-timestamp'
+
+class JobItem extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      expanded: false
+    }
+  }
+
+  render() {
+
+    const job = this.props.job
+
+    return(
+      <div key={ job.id } className = 'job container container--vertical'>
+        <div className = 'toprow'>
+          <p className = 'title'>{ job.title }</p>
+          <p className = "topright">  <TimeStamp time = { job.created_at }  /></p>
+
+        </div>
+
+        <p> <strong>{ job.company }</strong> - { job.suburb}</p>
+
+        { job.formatted_salary && <p>Salary: ${ job.formatted_salary}</p> }
+
+        <p style = { {textTransform: 'capitalize'} }> { job.work_type }</p>
+
+        { this.state.expanded ?
+          <div>
+            <p>{ job.description }</p>
+            <a href = { job.source_link } target = '_blank' rel = 'noopener noreferrer'> See original listing</a>
+          </div> :
+          <div>
+            <p>{ job.description.split(' ').slice(0, 10).join(' ') }...</p>
+
+          </div>
+
+        }
+
+
+        <span className = 'show-more' onClick = { () => { this.setState({ expanded: !this.state.expanded }) } }>
+          { this.state.expanded ? '- show less' : '+ read more' }
+        </span>
+
+      </div>
+    )
+  }
+}
+
+export default JobItem
--- src/components/JobList.js
+++ src/components/JobList.js
@@ -1,40 +1,18 @@
 import React from 'react'
-import TimeStamp from 'react-timestamp'
+import JobItem from './JobItem'
+
 
 const JobList = ({ jobs }) => (
   <div className = 'container container--vertical'>
-
-    <span className = 'count-jobs'>Your search has returned {jobs.length} job{(jobs.length !== 1) && 's'}.</span>
-
+    <span className = 'count-jobs'>Your search has returned { jobs.length } job{ (jobs.length !== 1) && 's' }.</span>
     <br />
 
     {
         jobs.map((job) => (
-          <div key={job.id} className = 'job container container--vertical'>
-
-            {/* <div className = 'toprow'>
-              <p className = 'title'>{ job.title }</p>
-              <p className = "topright"> { job.work_type}</p>
-            </div> */}
-
-            <div className = 'toprow'>
-              <p className = 'title'>{ job.title }</p>
-              <p className = "topright">  <TimeStamp time = {job.created_at}  /></p>
-
-            </div>
-
-            <p> <strong>{ job.company }</strong> - { job.suburb}</p>
-            {
-              job.formatted_salary && <p>Salary: ${ job.formatted_salary}</p>
-            }
-            <p>{ job.description}</p>
-
-            <a href = {job.source_link} target = '_blank' rel = 'noopener noreferrer'> See original listing</a>
-          </div>
+          <JobItem  key = { job.id } job = { job } />
         ))
     }
   </div>
-
 )
 
 export default JobList
