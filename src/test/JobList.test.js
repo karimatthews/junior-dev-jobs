94a2c4a5a21b0c5b95ab96246322f864a7028535
Kari Matthews <karimatthews3@gmail.com>
2017-08-15 16:30:07 +1000

    test that JobList renders a JobItem

--- src/test/JobList.test.js
+++ src/test/JobList.test.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import JobList from '../components/JobList';
-
-
+import JobItem from '../components/JobItem';
 
 describe( 'JobList Component', () => {
   describe( 'When there are no jobs', () => {
-
     it('renders zero jobs message', () => {
       const wrapper = shallow(<JobList />);
       const message = <span className = 'count-jobs'>Your search has returned 0 jobs.</span>;
@@ -16,13 +14,13 @@ describe( 'JobList Component', () => {
   })
 
   describe( 'When there are jobs', () => {
-
-
     it('renders zero jobs message', () => {
       const wrapper = shallow(<JobList jobs={[{id: 1}]}/> );
       const message = <span className = 'count-jobs'>Your search has returned 1 job.</span>;
+      const jobitem = <JobItem  key = '1' job = { {id: 1} } />
       // expect(wrapper.contains(welcome)).to.equal(true);
       expect(wrapper.contains(message)).toEqual(true);
+      expect(wrapper.contains(jobitem)).toEqual(true);
     });
   })
 })
