c0f1e6ba2002a46e6dea422947a9af8a1017b468
MattSegal <mattdsegal@gmail.com>
2017-10-02 22:06:18 +1100

    refactor job filter component

--- src/components/SearchJobs.js
+++ src/components/SearchJobs.js
@@ -1,22 +1,51 @@
-
 import React, {Component} from 'react'
 import uniq from 'lodash/uniq'
 import includes from 'lodash/includes'
 import JobFetcher from './JobFetcher'
 import JobList from './JobList'
 
+const JOB_TYPES = {
+  internship: 'Internship',
+  fulltime: 'Full time',
+  parttime: 'Part time',
+  contract: 'Contract',
+  casual: 'Casual',
+  unknown: 'Unknown'
+}
 
-
-
-class FilterJobs extends Component{
+class FilterJobs extends Component {
   constructor() {
     super()
     this.state = {
       search: "",
-      work_types: {unknown: true, internship: true, fulltime: true, parttime: true, casual: true, contract: true},
+      workTypes: Object.keys(JOB_TYPES).reduce((acc, val) => {acc[val] = true; return acc}, {}),
       suburb: "All suburbs"
     }
   }
+
+  onCheckboxChecked = checkboxName => event => {
+    this.setState({
+      workTypes: { 
+        ...this.state.workTypes, 
+        [checkboxName]: event.target.checked
+      }
+    })
+  }
+
+  onSearchInput = event => {
+    this.setState({search: event.target.value})
+  }
+
+  onSelectSuburb = event => {
+    this.setState({suburb: event.target.value})
+  }
+
+  getUniqueSuburbs() {
+    if (!this.props.jobs) return []
+    return uniq(this.props.jobs.map(job => job.suburb))
+      .filter(suburb => suburb)
+  }
+
   render() {
     let filteredJobs
     if (this.props.jobs == null) {
@@ -24,7 +53,7 @@ class FilterJobs extends Component{
     } else {
      filteredJobs = this.props.jobs.filter((job) => {
         const matchesSearch = includes(JSON.stringify(job).toLowerCase(), this.state.search.toLowerCase())
-        const matchesWorkType = this.state.work_types[job.work_type.split(' ').join('')]
+        const matchesWorkType = this.state.workTypes[job.work_type.split(' ').join('')]
         const matchesSuburb = (this.state.suburb === 'All suburbs' || includes(this.state.suburb, job.suburb))
         return matchesWorkType && matchesSearch && matchesSuburb
       })
@@ -34,94 +63,47 @@ class FilterJobs extends Component{
         <div className = ' container container--vertical filter'>
           <h3>Search Jobs</h3>
           <br/>
+
           <div className = 'flex-row' >
-            <input type="text" className="form-control search form-control--narrow" placeholder="Search" value = {this.state.search} onChange = {
-              (event) => {
-                this.setState({search: event.target.value})
-              }
-            } />
+            <input type="text" 
+              className="form-control search form-control--narrow" 
+              placeholder="Search" value = {this.state.search} 
+              onChange={this.onSearchInput} 
+            />
           </div>
           <br/>
+
           <strong> Work type: </strong>
-
-          <div className="checkbox">
-            <label><input type="checkbox" checked={this.state.work_types.internship} onChange = {
-              (event) => {
-                this.setState({work_types: { ...this.state.work_types, internship: event.target.checked}})
-              }
-            }/> Internship</label>
-          </div>
-
-          <div className="checkbox">
-            <label><input type="checkbox" checked={this.state.work_types.fulltime} onChange = {
-              (event) => {
-                this.setState({work_types: { ...this.state.work_types, fulltime: event.target.checked}})
-              }
-            }/> Full Time</label>
-          </div>
-
-          <div className="checkbox">
-            <label><input type="checkbox" checked={this.state.work_types.parttime} onChange = {
-              (event) => {
-                this.setState({work_types: { ...this.state.work_types, parttime: event.target.checked}})
-              }
-            }/> Part Time</label>
-          </div>
-
-          <div className="checkbox">
-            <label><input type="checkbox" checked={this.state.work_types.contract} onChange = {
-              (event) => {
-                this.setState({work_types: { ...this.state.work_types, contract: event.target.checked}})
-              }
-            }/> Contract</label>
-          </div>
-
-          <div className="checkbox">
-            <label><input type="checkbox" checked={this.state.work_types.casual} onChange = {
-              (event) => {
-                this.setState({work_types: { ...this.state.work_types, casual: event.target.checked}})
-              }
-            }/> Casual</label>
-          </div>
-
-          <div className="checkbox">
-            <label><input type="checkbox" checked={this.state.work_types.unknown} onChange = {
-              (event) => {
-                this.setState({work_types: { ...this.state.work_types, unknown: event.target.checked}})
-              }
-            }/> Unknown</label>
-          </div>
-
+          {Object.entries(JOB_TYPES).map(([key, title]) => (
+            <div className="checkbox" key={key}>
+              <label>
+                <input type="checkbox" 
+                  checked={this.state.workTypes[key]} 
+                  onChange={this.onCheckboxChecked(key)}
+                />
+                {title}
+              </label>
+            </div>
+          ))}
           <br/>
 
           <div className="form-group">
             <label>Suburb:</label>
-            <select className="form-control suburb" value = {this.state.suburb} onChange = {
-              (event) => {
-                this.setState({suburb: event.target.value})
-              }
-            }>
+            <select className="form-control suburb" 
+              value={this.state.suburb} 
+              onChange={this.onSelectSuburb}>
               <option> All suburbs </option>
-              {
-                this.props.jobs
-                &&
-                uniq(this.props.jobs.map((job) => (
-                  job.suburb
-                ))).map((suburb) => (
-                  <option key= {suburb}> {suburb} </option>
-                ))
-              }
+              {this.getUniqueSuburbs().map(
+                suburb => <option key={suburb}> {suburb} </option>
+              )}
             </select>
           </div>
-
         </div>
 
         {this.props.jobs ? <JobList jobs={filteredJobs} /> : <div> Loading... </div> }
-
       </div>
   )
 }}
-
 
 
 const SearchJobs = () => (
